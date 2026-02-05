0d50ff4a5d120a6ad20ffe893b117ab882f6f0af
Yesmurat Sagyndyk <yesmurat2006@gmail.com>
2026-02-04 21:27:08 -0500

    [rv32.ssimulation result

--- tests/rv32.s
+++ tests/rv32.s
@@ -2,36 +2,36 @@
 .text
 
 _boot:
-    addi x1, x0, 10        # x1 = 10
-    addi x2, x0, -5        # x2 = -5
-    addi x3, x1, 20        # x3 = 30
+    addi x1, x0, 10        # x1 = 10 (passed)
+    addi x2, x0, -5        # x2 = -5 (passed)
+    addi x3, x1, 20        # x3 = 30 (passed)
 
     # Register arithmetic
-    add  x4, x1, x3        # x4 = 10 + 30 = 40
-    sub  x5, x4, x1        # x5 = 40 - 10 = 30
+    add  x4, x1, x3        # x4 = 10 + 30 = 40 (passed)
+    sub  x5, x4, x1        # x5 = 40 - 10 = 30 (passed)
 
     # Logical ops
-    and  x6, x4, x3        # x6 = 40 & 30
-    or   x7, x4, x3        # x7 = 40 | 30
-    xor  x8, x4, x3        # x8 = 40 ^ 30
+    and  x6, x4, x3        # x6 = 40 & 30 (passed)
+    or   x7, x4, x3        # x7 = 40 | 30 (passed)
+    xor  x8, x4, x3        # x8 = 40 ^ 30 (passed)
 
     # Shifts (lower 5 bits used for RV32)
-    slli x9,  x1, 2        # x9 = 10 << 2 = 40
-    srli x10, x9, 1        # x10 = 40 >> 1 = 20 (logical)
-    srai x11, x2, 1        # x11 = -5 >> 1 (arithmetic) (fail)
+    slli x9,  x1, 2        # x9 = 10 << 2 = 40 (passed)
+    srli x10, x9, 1        # x10 = 40 >> 1 = 20 (logical) (passed)
+    srai x11, x2, 1        # x11 = -5 >> 1 (arithmetic) (fail: rdW = 11, ResultW = 7ffffffd)
 
     # Comparisons
-    slt  x12, x2, x1       # x12 = 1 (-5 < 10) (fail)
-    sltu x13, x2, x1       # unsigned compare (x13 = 0)
+    slt  x12, x2, x1       # x12 = 1 (-5 < 10) (fail: rdW = 12, ResultW = ffffffff)
+    sltu x13, x2, x1       # unsigned compare (x13 = 0) (passed)
 
     # Load upper immediate
-    lui  x14, 0x12345      # x14 = 0x12345000 (fail) ...
-    addi x14, x14, 0x678   # x14 = 0x12345678
+    lui  x14, 0x12345      # x14 = 0x12345000 (fail: rdW = 0, ResultW = 0x0)
+    addi x14, x14, 0x678   # x14 = 0x12345678 (fail: rdW = 14, ResultW = 0x678) (instruction executed correctly based on the previous instruction)
 
     # Memory test
-    addi x15, x0, 0x100 # base address
-    sw   x14, 0(x15)    # store word
-    lw   x16, 0(x15)    # load word
+    addi x15, x0, 0x100 # base address (passed)
+    sw   x14, 0(x15)    # store word (fail: rdM = X, WriteDataM = 0x678)
+    lw   x16, 0(x15)    # load word (fail: rdW = 0, ResultW = 0x100)
 
     # Branches
     beq  x14, x16, equal32
